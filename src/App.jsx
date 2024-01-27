f584d5ba448e36c3234c7a80a1ff6deb098cf908
Kevin Daysal <kdaysal123@gmail.com>
2024-01-27 18:29:51 -0500

    Just show a hardcoded list

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Items from './components/Items'
 
 export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React hello!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Vite + React Practice!</h1>
+      <Items />
     </>
   )
 }
 
 //export default App
+/*
+<button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+*/
---
+++ src/components/Items.jsx
@@ -0,0 +1,19 @@
+
+
+const Items = () => {
+
+    let listItems = ['Item 1', 'Item 2', 'Item 3']
+
+
+    return (
+
+        <div>
+            <h2>List Items:</h2>
+            <p>{listItems}</p>
+        </div>
+
+
+    )
+} // end Items component
+
+export default Items;
