70c7d27b7aa3d5b3229b66ee55b40805a805d422
Kevin Daysal <kdaysal123@gmail.com>
2024-01-27 18:52:07 -0500

    testing input and button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,28 @@ import './App.css'
 import Items from './components/Items'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [listItems, setListItems] = useState([])
+
+  const handleSubmit = (e) => {
+    console.log('input submitted!')
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
+
+
 
   return (
     <>
       <h1>Vite + React Practice!</h1>
+      <label htmlFor="userInput">Add List Items:</label>
+      <input
+        type="text"
+        id="userInput"
+        onChange={handleChange}
+      ></input>
+      <button onClick={handleSubmit}>Add it!</button>
       <Items />
     </>
   )
