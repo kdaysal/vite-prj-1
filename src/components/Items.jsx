7066a6eef1c64232fe6b7d3dd275030aca169800
Kevin Daysal <kdaysal123@gmail.com>
2024-01-28 00:24:26 -0500

    Working map function

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,5 +1,5 @@
 
-
+/*
 const Items = ({ listItems }) => {
 
     return (
@@ -9,6 +9,25 @@ const Items = ({ listItems }) => {
                 {listItems.map((listItem, index) => {
                     return <p key={index}>{listItem}</p>
                 })}
+            </div>
+        </>
+    )
+} // end Items component
+
+export default Items;
+*/
+//BEGIN CODE
+const Items = ({ listItems }) => {
+
+    let listItemCard = listItems.map((listItem, index) => {
+        return <p key={index}>{listItem}</p>
+    })
+
+    return (
+        <>
+            <h2>List Items...</h2>
+            <div>
+                {listItemCard}
             </div>
         </>
     )
