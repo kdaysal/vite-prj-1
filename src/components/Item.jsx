6e368a7ad5bf9199da4bef56e093c4a732de51e7
Kevin Daysal <kdaysal123@gmail.com>
2024-02-03 20:12:38 -0500

    Raised 'Item' component from the dead - now it is working

---
+++ src/components/Item.jsx
@@ -0,0 +1,9 @@
+//import Items from "./Items"
+
+const Item = ({ listItem }) => {
+    return (
+        <p>{listItem}</p>
+    )
+}
+
+export default Item
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,10 +1,22 @@
+// const Items = ({ listItems }) => {
+//     const listItemCard = listItems.map((listItem, index) => {
+//         return <p key={index}>{listItem}</p>
+//     })
+
+//     return (
+//         listItemCard
+//     )
+// }
+// export default Items
+
+import Item from "./Item"
+
 const Items = ({ listItems }) => {
-    const listItemCard = listItems.map((listItem, index) => {
-        return <p key={index}>{listItem}</p>
-    })
 
     return (
-        listItemCard
+        listItems.map((listItem, index) => {
+            return <Item key={index} listItem={listItem} />
+        })
     )
 }
 export default Items
